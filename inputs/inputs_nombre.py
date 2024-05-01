646331c911de261bdc783bead769fa352d5fe945
alu0101571669 <alu0101571669@ull.edu.es>
2024-05-01 19:04:45 +0100

    pedir parámetros en consola

---
+++ inputs/inputs_nombre.py
@@ -0,0 +1,3 @@
+# Pedirle un dato al usuario
+nombre = input("¿Cuál es tu nombre? ")
+print("Tu nombre es " + nombre) # Mostrar el nombre
---
+++ inputs/inputs_numeros.py
@@ -0,0 +1,13 @@
+# Pedir un número al usuario
+numero = input("Dame un número: ") # El número se guarda como string
+
+# Convertir el número a entero y lo múltiplico por 2
+resultado = (int(numero) * 2)
+
+# Convertir el número a flotante y lo múltiplico por 2
+resultado2 = (float(numero) * 2)
+
+# IMPORTANTE: Si trabajamos con números decimales trabajar con float y sino con int porque al ejecutar dar error
+
+print(resultado)
+print(resultado2)
